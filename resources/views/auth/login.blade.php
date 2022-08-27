d1c5391d0e9cbe02f2cef38412a23a4192a56596
Dhoneil Angchangco <act.dangchangco@gmail.com>
2022-08-27 18:20:52 +0800

    public login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,17 +3,17 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" type="image/x-icon" href="{{ asset('AdminLTE/dist/img/salon-icon.png') }}">
+  <link rel="icon" type="image/x-icon" href="/AdminLTE/dist/img/salon-icon.png">
   <title>Booking System | Log in</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="{{ asset("AdminLTE/plugins/fontawesome-free/css/all.min.css") }}">
+  <link rel="stylesheet" href="/AdminLTE/plugins/fontawesome-free/css/all.min.css">
   <!-- icheck bootstrap -->
-  <link rel="stylesheet" href="{{ asset("AdminLTE/plugins/icheck-bootstrap/icheck-bootstrap.min.css") }}">
+  <link rel="stylesheet" href="/AdminLTE/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="{{ asset("AdminLTE/dist/css/adminlte.min.css") }}">
+  <link rel="stylesheet" href="/AdminLTE/dist/css/adminlte.min.css">
 </head>
 <body class="hold-transition login-page">
 
@@ -99,11 +99,11 @@
 
 
 <!-- jQuery -->
-<script src="{{ asset("AdminLTE/plugins/jquery/jquery.min.js") }}"></script>
+<script src="/AdminLTE/plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
-<script src="{{ asset("AdminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js") }}"></script>
+<script src="/AdminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
-<script src="{{ asset("AdminLTE/dist/js/adminlte.min.js") }}"></script>
+<script src="/AdminLTE/dist/js/adminlte.min.js"></script>
 
 
 </body>
