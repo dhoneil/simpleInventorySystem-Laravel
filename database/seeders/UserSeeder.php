10b4008918af8f459c6554098bbebb25c3d89627
Dhoneil Angchangco <act.dangchangco@gmail.com>
2022-08-27 18:27:14 +0800

    dymsa password

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,7 +18,7 @@ class UserSeeder extends Seeder
         [
             'name' => 'Dymsa Tarrosa',
             'email' => 'dymsa@gmail.com',
-            'password' => '$2y$10$KMjSzMsAQ/F7FOq6eLgR2.rKHQbOftgxH73wPmO0aT/FBw4FPMIBe',
+            'password' => '$2y$10$rcjGoY6S9xVH.xb/J8/R7unO32eDQ6a9.xPov16ftP.Tl/HsJO26i',
             'role_id' => 1
         ]);
         DB::table('user_information')->insertGetId(
