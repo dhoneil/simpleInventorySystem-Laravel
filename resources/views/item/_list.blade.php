fc2ed6ff2bfa3f6032280a007f45f840d7a985d6
Dhoneil Angchangco <act.dangchangco@gmail.com>
2022-09-03 22:42:31 +0800

    should see the total remaining items

--- resources/views/item/_list.blade.php
+++ resources/views/item/_list.blade.php
@@ -1,3 +1,8 @@
+@php
+    use Illuminate\Support\Facades\DB;
+    use App\Models\HelperModel;
+@endphp
+
 <table class="table table-bordered table-striped table-sm table-hover">
     <thead style="background-color:#343a40; color:white;">
         <tr>
@@ -6,6 +11,7 @@
             <th>Name</th>
             <th>Description</th>
             <th>Price</th>
+            <th>Qty</th>
             <th>
                 <i class="fas fa-cog"></i>
             </th>
@@ -19,6 +25,22 @@
                 <td>{{ $c->item_name }}</td>
                 <td>{{ $c->item_description }}</td>
                 <td>{{ $c->price }}</td>
+                <td>
+                    @php
+                        $total_in = DB::table('purchase_transactions')
+                                    ->join('items','items.item_id','=','purchase_transactions.item_id')
+                                    ->where('purchase_transactions.item_id','=',$c->item_id)
+                                    ->sum('purchase_transactions.qty');
+
+                        $total_out = DB::table('sale_transactions')
+                                    ->join('items','items.item_id','=','sale_transactions.item_id')
+                                    ->where('sale_transactions.item_id','=',$c->item_id)
+                                    ->sum('sale_transactions.qty');
+
+                        $total_remaining = $total_in - $total_out;
+                        echo $total_remaining;
+                    @endphp
+                </td>
                 <td>
                     <button class="btn btn-xs btn-success btnedit" title="Edit Customer"><i class="fas fa-edit"></i></button>
                     <button class="btn btn-xs btn-danger btndelete" title="Delete Customer"><i class="fas fa-trash"></i></button>
